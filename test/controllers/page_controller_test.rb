ea50c8a70e11d771ea8d7037ddbfe931f61e8941
assly <blue20will@gmail.com>
2018-06-16 20:48:35 +0900

    Improve about Rails's test

--- test/controllers/page_controller_test.rb
+++ test/controllers/page_controller_test.rb
@@ -1,8 +1,8 @@
 require 'test_helper'
 
 class PageControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get page_index_url
+  test "should get index as root" do
+    get root_url
     assert_response :success
   end
 
