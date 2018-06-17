6d4a7052b7a7772d5b283083dad42061f19fde08
assly <blue20will@gmail.com>
2018-06-17 23:37:16 +0900

    Add RESTful resources; create, update and destory

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,4 +1,13 @@
 class TodosController < ApplicationController
   def index
   end
+
+  def create
+  end
+
+  def update
+  end
+
+  def destory
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   root to: 'todos#index'
+
+  resources :todo, only: %i[create update destroy]
 end
