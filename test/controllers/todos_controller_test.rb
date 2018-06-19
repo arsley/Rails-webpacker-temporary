6fb187da7a7af2e89b2f0daedad138b771c7182e
assly <blue20will@gmail.com>
2018-06-19 22:35:21 +0900

    Add TodosController's test: asserts create action's response

--- test/controllers/todos_controller_test.rb
+++ test/controllers/todos_controller_test.rb
@@ -6,4 +6,15 @@ class TodosControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
+  test 'should return "400  Bad request" if send no-content params' do
+    invalid_params = { todo: { content: '' } }
+    post todos_url(invalid_params)
+    assert_response :bad_request
+  end
+
+  test 'should return "201 Created" if send valid params' do
+    valid_params = { todo: { content: 'Valid todo!' } }
+    post todos_url(valid_params)
+    assert_response :created
+  end
 end
