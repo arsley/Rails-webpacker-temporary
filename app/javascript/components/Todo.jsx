55556e6cd78e9dd9e2115d7d6085cfb84201f62a
assly <blue20will@gmail.com>
2018-06-19 16:52:18 +0900

    Create basic Todo components and passing data method

--- app/javascript/components/Todo.jsx
+++ app/javascript/components/Todo.jsx
@@ -1,12 +1,28 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
 import PropTypes from 'prop-types'
+
+function TodoItem(props) {
+  return (
+    <div className="todoitem">
+      <p className="todoitem-content">{props.content}</p>
+    </div>
+  );
+}
+
+function TodoList(props) {
+  const todos = JSON.parse(props.todos);
+  const list  = todos.map((todo, i) =>
+    <TodoItem key={i} content={todo.content} />
+  );
+  return list;
+}
+
 
 export default class Todo extends React.Component {
   render() {
     return (
       <div className="todo-wrapper">
-        <p className={'todoitem' + this.props.id}>Contents here.</p>
+        <TodoList todos={this.props.todos} />
       </div>
     );
   }
